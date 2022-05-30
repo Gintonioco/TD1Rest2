0419dc8da5587dc014ba2339a29f2c1ff6310cb4
rigol <rigolet.antoine@gmail.com>
2022-05-30 12:39:04 +0200

    Ajout de la fonction saveStudent

--- src/main/java/com/inti/TD1Rest2/controller/EtudiantController.java
+++ src/main/java/com/inti/TD1Rest2/controller/EtudiantController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,5 +42,11 @@ public class EtudiantController
 	public ResponseEntity<List<Etudiant>> getAllStudents()
 	{
 		return new ResponseEntity<List<Etudiant>>(etudiantRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/saveStudent")
+	public ResponseEntity<Etudiant> saveStudent(@RequestBody Etudiant etudiant)
+	{
+		return new ResponseEntity<Etudiant>(etudiantRepository.save(etudiant), HttpStatus.CREATED);
 	}
 }
