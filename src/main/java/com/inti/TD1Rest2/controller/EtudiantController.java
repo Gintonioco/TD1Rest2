8867ddd044a17cfedf36ee542539b6248ac833e7
rigol <rigolet.antoine@gmail.com>
2022-05-30 12:16:05 +0200

    Ajout de la fonction getAllStudents

--- src/main/java/com/inti/TD1Rest2/controller/EtudiantController.java
+++ src/main/java/com/inti/TD1Rest2/controller/EtudiantController.java
@@ -1,9 +1,13 @@
 package com.inti.TD1Rest2.controller;
 
+import java.util.List;
+
 import com.inti.TD1Rest2.model.Etudiant;
 import com.inti.TD1Rest2.repository.EtudiantRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +29,17 @@ public class EtudiantController
 	{
 		Etudiant e = new Etudiant("Jean");
 		etudiantRepository.save(e);
+	}
+	
+//	@GetMapping("/students")
+//	public List<Etudiant> displayAllStudents()
+//	{
+//		return etudiantRepository.findAll();
+//	}
+	
+	@GetMapping("/students")
+	public ResponseEntity<List<Etudiant>> getAllStudents()
+	{
+		return new ResponseEntity<List<Etudiant>>(etudiantRepository.findAll(), HttpStatus.OK);
 	}
 }
